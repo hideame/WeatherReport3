cf991364ac7960a3126f09b620ad671576c11c71
hideame <hamezawa@gmail.com>
2021-04-09 11:36:30 +0900

    アップロードした画像と入力した情報を表示させ、リスト表示したい

--- app/src/main/java/com/example/weatherreport3/ImagesActivity.java
+++ app/src/main/java/com/example/weatherreport3/ImagesActivity.java
@@ -2,7 +2,12 @@ package com.example.weatherreport3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImagesActivity extends AppCompatActivity {
 
@@ -11,5 +16,16 @@ public class ImagesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images);
 //        Log.d("hoge", "テスト");
+
+        // サンプルのデータを作成
+        List<Upload> uploads = new ArrayList<>();
+        uploads.add(new Upload("mNameSample", "mImageUrlSample", "mPlaceSample", "mYourNameSample"));
+        // サンプルのデータをAdapterに設定
+        ImageAdapter adapter = new ImageAdapter(this, uploads);
+
+        RecyclerView recyclerView = this.findViewById(R.id.recycler_view);
+        // recyclerView に Adapterを設定
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
